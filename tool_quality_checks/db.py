a576d07578f0657fc74ff1ffdeb3cb332c306cc0
arnoldvantveld <arnold.vantveld@nelen-schuurmans.nl>
2019-07-31 15:49:03 +0200

    get back pull request [work-in-progress] Arnold tool checks

---
+++ tool_quality_checks/db.py
@@ -0,0 +1,157 @@
+# (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.rst.
+# -*- coding: utf-8 -*-
+
+import psycopg2
+
+# from psycopg2.extras import RealDictCursor
+import logging
+import sql_checks
+import sql_views
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_FETCH = "default"
+
+
+class ThreediDatabase(object):
+    """
+    Connects to a database using python's psycopg2 module.
+    """
+
+    def __init__(self, settings):
+        """
+        Establishes the db connection.
+        """
+        # could be made variable
+        self.schema = "public"
+        credentials = {
+            "dbname": settings.database,
+            "host": settings.host,
+            "user": settings.username,
+            "password": settings.password,
+        }
+        try:
+            self.db = psycopg2.connect(**credentials)
+        except psycopg2.Error as e:
+            logger.exception(e)
+            raise
+
+    def initialize_db(self):
+        """
+            initialize database for checks
+        """
+        self.create_schema(schema_name="chk")
+        for view_table in [
+            ["public", "v2_1d_boundary_conditions_view"],
+            ["public", "v2_pumpstation_point_view"],
+            ["public", "v2_1d_lateral_view"],
+            ["public", "v2_cross_section_definition_rio_view"],
+            ["chk", "v2_pipe_view_left_join"],
+            ["chk", "v2_orifice_view_left_join"],
+            ["chk", "v2_weir_view_left_join"],
+        ]:
+            self.create_view(view_table=view_table[1], view_schema=view_table[0])
+
+    def get_count(self, table_name):
+        """
+        :param table:
+        :return:
+        """
+        statement = "SELECT COUNT(*) from {schema}.{table_name:s}".format(
+            table_name=table_name, schema=self.schema
+        )
+        result = self.free_form(statement, fetch=True)
+        return result[0][0]
+
+    def _get_cursor(self, user_choise=""):
+        # if user_choise == 'dict':
+        #    return self.db.cursor(cursor_factory=RealDictCursor)
+        # else:
+        return self.db.cursor()
+
+    def _raise(self, _exception):
+        if self.db:
+            self.db.rollback()
+        logger.error("Error %s" % _exception)
+        raise
+
+    def free_form(self, sql_statement, fetch=True, fetch_as=DEFAULT_FETCH):
+        """
+        :param sql_statement: custom sql statement
+
+        makes use of the existing database connection to run a custom query
+        """
+
+        try:
+            cur = self._get_cursor(fetch_as)
+            cur.execute(sql_statement)
+            self.db.commit()
+            logger.debug("[+] Successfully executed statement {}".format(sql_statement))
+            if fetch is True:
+                return cur.fetchall()
+
+        except psycopg2.DatabaseError as e:
+            self._raise(e)
+
+    def select_table_names(self, search_table_name, schema=None):
+        schema = schema or self.schema
+
+        statement = """
+        SELECT table_name
+        FROM   information_schema.tables
+        WHERE  table_schema = '{schema}'
+        AND    table_name LIKE '{search_table_name}'
+        AND    table_type = 'BASE TABLE';
+        """.format(
+            schema=schema, search_table_name=search_table_name
+        )
+        return [i[0] for i in self.free_form(statement, fetch=True)]
+
+    def create_schema(self, schema_name, drop_schema=False):
+        """create a schema"""
+        if drop_schema == True:
+            drop_schema = """DROP SCHEMA IF EXISTS {schema_name} CASCADE;""".format(
+                schema_name=schema_name
+            )
+            self.free_form(drop_schema, fetch=False)
+        create_schema_statement = """
+        CREATE SCHEMA IF NOT EXISTS {schema_name}
+        ;""".format(
+            schema_name=schema_name
+        )
+        self.free_form(sql_statement=create_schema_statement, fetch=False)
+
+    def populate_geometry_columns(self):
+        """Populate geometry columns"""
+        populate_geometry_columns_statement = """
+        SELECT Populate_Geometry_Columns();"""
+        self.free_form(sql_statement=populate_geometry_columns_statement, fetch=False)
+
+    def perform_checks_with_sql(self, settings, check_table, check_type):
+        """
+        Performs quality checks on postgres DB
+
+        :param check_table - list of one or more structure tables (e.g. v2_manhole)
+        :param check_type - select type of check: completeness, quality
+        """
+        check_table = check_table.replace("v2_", "")
+        sql_template_name = "sql_" + check_type + "_" + check_table
+        if sql_template_name in sql_checks.sql_checks:
+            statement = sql_checks.sql_checks[sql_template_name].format(
+                **settings.__dict__
+            )
+            self.free_form(sql_statement=statement, fetch=False)
+
+    def create_view(self, view_table, view_schema, drop_view=True):
+        """
+        Creates a view with a join to v2_connection_nodes table
+        
+        :param view_table - table of which the view is created
+        """
+        if drop_view == True:
+            drop_statement = """DROP VIEW IF EXISTS {view_table};""".format(
+                view_table=view_table
+            )
+            self.free_form(drop_statement, fetch=False)
+        create_statement = sql_views.sql_views[view_table].format(schema=view_schema)
+        self.free_form(sql_statement=create_statement, fetch=False)
---
+++ tool_quality_checks/quality_checks.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+""" TODO Docstring. """
+import os
+import argparse
+import logging
+
+from configparser import RawConfigParser
+from db import ThreediDatabase
+
+logger = logging.getLogger(__name__)
+
+
+def quality_checks(settings):
+    """Overall function for checking our model data"""
+
+    # get database connection
+    db = ThreediDatabase(settings)
+    db.initialize_db()
+
+    # get v2_table_names
+    v2_table_names = db.select_table_names("v2%")
+    for table_name in v2_table_names:
+        if db.get_count(table_name) > 0:
+            print(table_name, db.get_count(table_name))
+            db.perform_checks_with_sql(settings, table_name, check_type="completeness")
+            db.perform_checks_with_sql(settings, table_name, check_type="quality")
+
+    db.populate_geometry_columns()
+
+
+def resolve_ini(custom_ini_file):
+    """
+    decide which ini file to use
+    """
+    # get default ini for testing purposes
+    default_ini_relpath = "test\\data\\instellingen_test.ini"
+    default_ini_relpath = os.path.join(os.path.dirname(__file__), default_ini_relpath)
+    if custom_ini_file is None:
+        logger.info(
+            "[*] Using default ini file {}".format(
+                os.path.basename(default_ini_relpath)
+            )
+        )
+        return default_ini_relpath
+    if all((os.path.exists(custom_ini_file), os.path.isfile(custom_ini_file))):
+        logger.info(
+            "[*] Using custom ini file {}".format(os.path.basename(custom_ini_file))
+        )
+        return custom_ini_file
+    else:
+        raise ("Error: Could not find the ini file {}".format(custom_ini_file))
+
+
+class settingsObject(object):
+    """Contains the settings from the ini file"""
+
+    def __init__(self, inifile):
+        config = RawConfigParser()
+        config.read(inifile)
+
+        # inifile
+        self.ini = inifile
+
+        for section in config.sections():
+            for key, value in config.items(section):
+                setattr(self, key, value)
+
+
+def get_parser():
+    """ Return argument parser. """
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        dest="verbose",
+        default=False,
+        help="Verbose output",
+    )
+    parser.add_argument(
+        "-i",
+        "--inifile",
+        metavar="INIFILE",
+        dest="inifile",
+        help="Location with settings ini for quality checks",
+    )
+
+    return parser
+
+
+def main():
+    """ Call command with args from parser. """
+    kwargs = vars(get_parser().parse_args())
+    if kwargs["verbose"]:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+    logging.basicConfig(level=log_level, format="%(levelname)s: %(message)s")
+    ini_relpath = resolve_ini(kwargs["inifile"])
+    print(ini_relpath)
+    settings = settingsObject(ini_relpath)
+    quality_checks(settings)
+
+
+if __name__ == "__main__":
+    exit(main())
